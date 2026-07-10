c99a823a6e0f8788140c30dcf81a867a79eb60fa
bwmp <oli@bwmp.dev>
2026-07-09 18:29:32 -0700

    feat: add SDK documentation for Eris, Oceanic.js, Sapphire, Hikari, disnake, nextcord, interactions.py, and Py-cord

--- content/sdks/_meta.js
+++ content/sdks/_meta.js
@@ -1,6 +1,14 @@
 export default {
   index: 'Overview',
   discordjs: 'discord.js (JS/TS)',
+  eris: 'Eris (JS/TS)',
+  oceanic: 'Oceanic.js (JS/TS)',
+  sapphire: 'Sapphire (JS/TS)',
   discordpy: 'discord.py (Python)',
+  nextcord: 'nextcord (Python)',
+  disnake: 'disnake (Python)',
+  pycord: 'Py-cord (Python)',
+  hikari: 'hikari (Python)',
+  interactions: 'interactions.py (Python)',
   go: 'discordgo (Go)'
 }
